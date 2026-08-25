bbaf14f1d80028213f5fb54e9fdc878a585596a2
JYCHOI02 <je0.0408@gmail.com>
2026-08-25 09:26:59 +0900

    Update game.js

--- game.js
+++ game.js
@@ -478,21 +478,19 @@ const pathTiles = [
     { col: 0, row: 4 },
     { col: 1, row: 4 },
     { col: 2, row: 4 },
-    { col: 3, row: 4 },
+
+    { col: 2, row: 3 },
 
     { col: 3, row: 3 },
-    { col: 3, row: 2 },
+    { col: 4, row: 3 },
+    { col: 5, row: 3 },
+    { col: 6, row: 3 },
 
-    { col: 4, row: 2 },
-    { col: 5, row: 2 },
-    { col: 6, row: 2 },
-    { col: 7, row: 2 },
-    { col: 8, row: 2 },
+    { col: 6, row: 4 },
+    { col: 6, row: 5 },
 
-    { col: 8, row: 3 },
-    { col: 8, row: 4 },
+    { col: 7, row: 5 },
     { col: 8, row: 5 },
-
     { col: 9, row: 5 },
     { col: 10, row: 5 },
     { col: 11, row: 5 },
